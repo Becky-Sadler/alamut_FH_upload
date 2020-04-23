a88d872d50f7063cbfa43fb74db6a6fb8880e279
Becky-Sadler <57992319+Becky-Sadler@users.noreply.github.com>
2020-04-23 18:04:09 +0100

    Editing to make it actually work correctly

--- alamut_upload1.0.py
+++ alamut_upload1.0.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np 
 
 # create a dataframe from the data (after converting it to a csv file in excel)
 df = pd.read_csv('FH_variants.csv', encoding='ISO-8859-1')
@@ -22,16 +21,16 @@ for index, row in df.iterrows():
                         Pathogenic = 'Class ' + str(row['acmg_classification']) #removing new lines
                 else:
                         Pathogenic = 'Class 3'
-                if row['comment'] == None:
-                        string = ' '
+                if pd.isnull(row['comment']) == True:
+                        string = ''
                 else:
                         string = str(row['comment']).replace('\n', ' ').replace('\r', '')
-                if row['criteria'] == None:
-                        criteria = ' '
+                if pd.isnull(row['criteria']) == True:
+                        criteria = ''
                 else: 
-                        criteria = str(row['criteria'])
+                        criteria = str(row['criteria']) + ': '
                 # Combining evidence column with the comment column to add to the general Alamut comment column (separated with a : as cannot do a new line separation)
-                Comment = criteria + string + ' Date Assigned:' + str(row['date_assigned']) +  ' Date Checked:' + str(row['date_checked']) +  ' Date Reviewed:' + str(row['date_reviewed ']) + ' Seen :' + str(row['seen'])  
+                Comment = criteria + string + ' Date Assigned :' + str(row['date_assigned']) +  ' Date Checked: ' + str(row['date_checked']) +  ' Date Reviewed: ' + str(row['date_reviewed ']) + ' Seen :' + str(row['seen'])  
                 # Append this record to the cNomen dataframe 
                 cNomen_df = cNomen_df.append({'Gene': Gene, 'Transcript': Transcript, 'Variant' : cNomen, 'Pathogenic': Pathogenic, 'Patient ID' : PatientID, 'Comment' : Comment}, ignore_index=True)
 
@@ -49,4 +48,5 @@ LDLRAP1_df.to_csv('FH_LDLRAP1.txt', sep='\t', index = False)
 
 PCSK9_df = cNomen_df.loc[(cNomen_df['Gene'] == 'PCSK9')]
 PCSK9_df.to_csv('FH_PCSK9.txt', sep='\t', index = False)
+
 
